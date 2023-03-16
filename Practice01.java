de4243b64012670080acd4fd602a03a97a000f94
anjali0110 <54007853+anjali0110@users.noreply.github.com>
2023-03-16 02:27:51 +0000

    Add files via upload

---
+++ Practice01.java
@@ -0,0 +1,23 @@
+package mathdemo.mathRandomNumber;
+
+import java.util.Scanner;
+
+public class Practice01 {
+    public static void main(String[] args) {
+// 10,20,30
+        Scanner  scanner= new Scanner(System.in);
+        System.out.println("Enter the value of x: ");
+        double x = scanner.nextDouble();
+        System.out.println("Enter the value of y: ");
+        double y = scanner.nextDouble();
+        System.out.println("Enter the value of z: ");
+        double z = scanner.nextDouble();
+        double result =0;
+        result =Math.cbrt(Math.pow(x,2)+Math.pow(y,2)-Math.abs(z));
+        System.out.println(result);
+        scanner.close();
+
+
+
+    }
+}
